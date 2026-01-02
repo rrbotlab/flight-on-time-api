987ed1e76dbd3ca450e73b15113667b71b6604e2
tirsoalc <tirsonetogz@gmail.com>
2026-01-02 18:29:00 -0300

    fix: remove duplicated cors config and fix env name misspell

--- src/main/java/com/flightontime/flightapi/infra/security/CorsConfig.java
+++ src/main/java/com/flightontime/flightapi/infra/security/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed.origins}")
+    @Value("${app.cors.allowed-origins}")
     private String[] allowedUrlOrigins;
 
     @Bean
